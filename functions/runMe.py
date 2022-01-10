f02f8350424e735a3fcf8c5bc03296b47717cb6e
Shlomi Borovitz <shlomib@altostra.com>
2022-01-10 17:57:37 +0200

    Code translated

---
+++ functions/runMe.py
@@ -0,0 +1,98 @@
+import boto3
+import os
+
+message = '''
+
+Welcome to Altostra.
+
+To set your name, send a request to POST /my-name with your name as the body.
+
+See the README file for more detailed instructions on how to run this project locally and how to deploy it.
+
+Happy Clouding!
+'''
+
+region = os.environ.get('AWS_REGION')
+is_local = os.environ.get('AWS_SAM_LOCAL')
+table_name = os.environ.get('TABLE_QUERYME01')
+
+dynamo_db = isLocal if None else boto3.client('dynamodb')
+dynamoDb = !isLocal && new aws.DynamoDB.DocumentClient({ region })
+
+def handler(event, context):
+    if event['httpMethod'] == 'GET':
+        try:
+            return {
+                'statusCode': 200,
+                'body': get_message(),
+            }
+        except BaseException:
+            return {
+                'statusCode': 500,
+                'body': 'Unable to get the message due to a serverless error.',
+            }
+    elif event['httpMethod'] == 'POST':
+        try:
+            set_name(event['body'])
+            return {
+                'statusCode': 204,
+            }
+        except BaseException:
+            return {
+                'statusCode': 500,
+                'body': 'Unable to set your name due to a serverless error.',
+            }
+    else:
+        return {
+            'statusCode': 400,
+            'body': "It seems that you didn't provide the correct parameters :("
+        }
+
+def get_message():
+    greeting = 'Hi!'
+
+    if isLocal:
+        return greeting + message
+
+    try:
+        response = dynamo_db.get_item(
+            TableName=table_name,
+            Key={
+                'pk': {
+                    'S': 'SINGLETON',
+                },
+            }
+        )
+
+        greeting = (
+            response['Item'] && response['Item']['name']
+                if 'Hi ' + response['Item']['name'] + '!'
+                else 'Hi!'
+        )
+
+        return greeting + message
+    except BaseException as err:
+        msg = 'Failed to get name from storage.'
+        print(message, err)
+
+        raise
+
+def set_name(name):
+    if is_local:
+        return
+    
+    try:
+        if type(name) != str:
+            raise TypeError('Provided name is invalid.')
+
+        dynamo_db.put_item(
+            TableName=table_name,
+            Item={
+                'pk': { 'S': 'SINGLETON' },
+                'name': {'S': name},
+            },
+        )
+    except BaseException as err:
+        msg = 'Failed to store the name is storage.'
+        print(msg, err)
+        raise
